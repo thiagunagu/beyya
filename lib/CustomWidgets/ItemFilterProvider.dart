7d7f429db92030a290d6127b36766f581ad7ace2
thiagunagu <thiagunagu@gmail.com>
2021-02-20 23:49:26 -0600

    Added extra padding in search bar

--- lib/CustomWidgets/ItemFilterProvider.dart
+++ lib/CustomWidgets/ItemFilterProvider.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-
-import 'package:provider/provider.dart';
-
-import 'package:beyya/Models/UserDocument.dart';
-
 class ItemFilterProvider extends ChangeNotifier {
   String itemFilter = '';
   void changeItemFilter({newValue}) {
